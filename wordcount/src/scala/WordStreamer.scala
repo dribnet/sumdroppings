de4a2dc3e85c474d6b765274b8638f22411c9c81
Tom White <tom@sixdozen.com>
2013-09-16 01:53:04 -0700

    Added some code to WordStreamer.main based on Oscar's suggestions

--- wordcount/src/scala/WordStreamer.scala
+++ wordcount/src/scala/WordStreamer.scala
@@ -1,8 +1,11 @@
 package net.drib.sumdroppings.wordcount
 
 import com.twitter.summingbird._
-import com.twitter.summingbird.scalding.{ WaitingState, Scalding, ScaldingStore, PipeFactory }
-import com.twitter.scalding.{Local}
+// import com.twitter.summingbird.scalding.{ WaitingState, Scalding, ScaldingStore, PipeFactory }
+// import com.twitter.scalding.{Local, TextLine}
+import com.twitter.summingbird.scalding._
+import com.twitter.scalding._
+import com.twitter.summingbird.batch._
 import java.util.Date
 
 object WordStreamer {
@@ -15,10 +18,17 @@ object WordStreamer {
         .sumByKey(store)
 
   def main(args: Array[String]) {
-    val src: Producer[Scalding, String] = null
-    val dst: ScaldingStore[String, Long] = null
+    val input = args(0)
+    val output = args(1)
+    println ("Running: " + input + " --> " + output)
+    val src: Producer[Scalding, String] =
+      Producer.source(Scalding.pipeFactoryExact[String]( _ => TextLine("pathToInput")))
+    val istore = VersionedBatchStore[String, Long](output, 3)(_._2)
+    implicit val batcher: Batcher = Batcher.ofHours(1)
+    val store = new InitialBatchedStore(batcher.currentBatch - 2L, istore)
+    //TODO: need a real WaitingState
     var ws: WaitingState[Date] = null
-    val counter = wordCount(src, dst)
+    val counter = wordCount(src, store)
     ws = new Scalding("wordcount").run(ws, Local(true), counter)
   }
 
